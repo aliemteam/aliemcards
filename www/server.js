e90d52048387c9c355a6359f99380cfc9cc57078
Jeremy Voros <jeremyvoros@gmail.com>
2016-10-31 11:55:32 -0600

    add open shift defaults

--- www/server.js
+++ www/server.js
@@ -1,27 +1,28 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
-var compression = require('compression');
-var path = require('path');
-var api = require('./routes/api');
-//var dbapi = require('./routes/dbapi');
+const express = require('express');
+const app = express();
+const compression = require('compression');
+const path = require('path');
+const api = require('./routes/api');
 
 // gzip everything
 app.use(compression());
 
 // serve static files
-app.use('/', express.static(__dirname + '/assets'));
+app.use('/', express.static(path.join(__dirname, '/assets')));
 
 // import api routes
 app.use('/api', api);
-//app.use('/dbapi', dbapi);
+
 
 // all other routes get served this
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'react-app', 'index.html'));
-})
+});
 
 // run it
-app.listen(3000, function () {
+const serverPort = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+const serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(serverPort, serverIpAddress, () => {
   console.log('Example app listening on port 3000!');
 });
