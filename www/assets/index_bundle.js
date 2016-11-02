c54545511a2d9370621b3796b8873467bef877db
Jeremy Voros <jeremyvoros@gmail.com>
2016-11-02 02:40:30 -0600

    some front page changes

--- www/assets/index_bundle.js
+++ www/assets/index_bundle.js
@@ -28044,7 +28044,34 @@
 	    _react2.default.createElement(
 	      'p',
 	      null,
-	      'This is where all the ALiEM Cards live. Formerly the PV Cards.'
+	      'Cornhole poke semiotics, direct trade banjo +1 next level. Four loko humblebrag prism jianbing. Tbh next level jean shorts fanny pack tattooed. Meggings bicycle rights pabst ethical, chartreuse tumblr woke tumeric godard activated charcoal truffaut plaid. Deep v put a bird on it normcore paleo food truck kombucha. Raw denim tofu helvetica pop-up direct trade fam. Fam plaid pour-over, jean shorts farm-to-table hot chicken fixie etsy wolf.'
+	    ),
+	    _react2.default.createElement(
+	      'p',
+	      null,
+	      'Hot chicken thundercats migas poutine distillery, blog occupy keffiyeh 3 wolf moon fingerstache four loko direct trade. Fingerstache mlkshk brooklyn, cray wayfarers whatever selfies drinking vinegar fashion axe before they sold out. Gastropub street art post-ironic YOLO sustainable, polaroid cliche portland locavore. Glossier forage migas put a bird on it. Glossier mumblecore banh mi, whatever franzen actually DIY. Poke kogi master cleanse, sustainable typewriter salvia hexagon whatever fap vinyl banh mi tofu yr. Celiac fap vice, copper mug ennui literally microdosing normcore.'
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      { className: 'row' },
+	      _react2.default.createElement(
+	        'div',
+	        { className: 'one-half column' },
+	        _react2.default.createElement(
+	          'h1',
+	          null,
+	          'Newest'
+	        )
+	      ),
+	      _react2.default.createElement(
+	        'div',
+	        { className: 'one-half column' },
+	        _react2.default.createElement(
+	          'h1',
+	          null,
+	          'Most Popular'
+	        )
+	      )
 	    )
 	  );
 	};
