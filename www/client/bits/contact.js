c6aaa31cb789442178b96730dc512e908d854923
Jeremy Voros <jeremyvoros@gmail.com>
2016-11-22 10:45:52 -0700

    more contact fixes

---
+++ www/client/bits/contact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Contact extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Contact Us</h1>
+      </div>
+    );
+  }
+
+};
+
+Contact.propTypes = {
+
+};
+
+Contact.defaultProps = {
+
+};
+
+export default Contact;
