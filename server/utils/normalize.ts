8b517733bd170bad49c94a712fbdb120d621950a
Jeremy Voros <jeremyvoros@gmail.com>
2017-03-23 13:12:48 -0600

    revert normalize.ts

--- server/utils/normalize.ts
+++ server/utils/normalize.ts
@@ -32,13 +32,10 @@ function createCategoryObj(category: string): Category {
 }
 
 export const normalize = (cards: SingleCardJSON[]): Context => {
-  const mapped = cards.map(card => {
-    console.log(`normalizing ${card.title}`);  
-    Object.assign({}, card, {
-      authors: card.authors.map(AuthorFactory.create),
-      categories: card.categories.map(createCategoryObj),
-    });
-  });
+  const mapped = cards.map(card => Object.assign({}, card, {
+     authors: card.authors.map(AuthorFactory.create),
+     categories: card.categories.map(createCategoryObj),
+   }));
   
   // const mapped = cards.map(card => ({...card,
   //   authors: card.authors.map(AuthorFactory.create),
