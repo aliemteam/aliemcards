2d960ec23b022accf4abada692b6c572fb11d16f
Derek P Sifford <dereksifford@gmail.com>
2017-04-09 20:03:12 -0400

    add more tests to SearchResults

--- app/components/__tests__/SearchResults-test.tsx
+++ app/components/__tests__/SearchResults-test.tsx
@@ -1,43 +1,68 @@
-// tslint:disable:no-console
 jest.mock('react-router-dom');
 jest.mock('react-apollo');
-jest.mock('react-router-dom');
 
 import { shallow } from 'enzyme';
 import * as React from 'react';
-// import * as renderer from 'react-test-renderer';
 import SearchResults from '../SearchResults';
+
+interface Card {
+  id: string;
+  title: string;
+}
+
+const click = jest.fn();
+const defaultCards = [
+  {
+      id: 'dvt',
+      title: 'DVT of the Leg - ACCP Guidelines',
+  },
+  {
+      id: 'dvt-ultrasound',
+      title: 'Ultrasound DVT Assessment',
+  },
+  {
+      id: 'pe-prediction-rules',
+      title: 'Pulmonary Embolism Clinical Prediction Rules',
+  },
+  {
+      id: 'ultrasound-measurements',
+      title: 'Ultrasound Measurement Cheat Sheet',
+  },
+];
+
+const setup = (cards: Card[] = defaultCards) => {
+  const data = {
+    cards,
+    networkStatus: 0,
+  };
+  const component = shallow(
+    <SearchResults query="test" data={data} onClick={click} />,
+  );
+  return {
+    component,
+  };
+};
 
 describe('<SearchResults />', () => {
   it('should render correctly', () => {
-    const data = {
-      cards: [
-        {
-            id: 'dvt',
-            title: 'DVT of the Leg - ACCP Guidelines',
-        },
-        {
-            id: 'dvt-ultrasound',
-            title: 'Ultrasound DVT Assessment',
-        },
-        {
-            id: 'pe-prediction-rules',
-            title: 'Pulmonary Embolism Clinical Prediction Rules',
-        },
-        {
-            id: 'ultrasound-measurements',
-            title: 'Ultrasound Measurement Cheat Sheet',
-        },
-      ],
-      networkStatus: 0,
-    };
-
-    const component = shallow(
-      <SearchResults query="test" data={data} onClick={() => console.log('click')} />,
-    );
-
+    const { component } = setup();
     expect(component.find('.search__result').length).toBe(4);
-
-    console.log(component.debug());
+  });
+  it('should throw an error if no data is available', () => {
+    const { component } = setup();
+    try {
+      component.setProps({data: null});
+    }
+    catch (err) {
+      expect(err.message).toBe('Data should always be available');
+      return;
+    }
+    throw new Error('Test should not reach this point');
+  });
+  it('should return early if result is empty', () => {
+    const { component } = setup();
+    expect(component.html()).not.toBeNull();
+    component.setProps({ data: { cards: null } });
+    expect(component.html()).toBeNull();
   });
 });
