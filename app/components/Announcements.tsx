da2e078fc2b732f7558686eb0ae328d534ab1388
Jeremy Voros <jeremyvoros@gmail.com>
2017-05-08 10:20:09 -0600

    style change

--- app/components/Announcements.tsx
+++ app/components/Announcements.tsx
@@ -66,7 +66,7 @@ export default class Announcements extends React.Component<Props, State> {
             </span>
             <button
               className="announcement_dismiss"
-              children="dismiss"
+              children="&times; dismiss"
               onClick={this.dismiss}
             />
           </div>
