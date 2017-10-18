1994afe435e3d78dd4ad43f1795ba9ac0c88f7f7
Derek Sifford <dereksifford@gmail.com>
2017-10-18 14:15:24 -0400

    Upgrade all dependencies + fix all broken things (#248)
    * update all dependencies + fix all broken things
    
    * fix script in package.json
    
    * use npm in scripts
    
    * upgrade dependencies

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 
-export default () =>
+export default () => (
   <div>
     <h3>404 page not found</h3>
     <p>We are sorry but the page you are looking for does not exist.</p>
-  </div>;
+  </div>
+);
